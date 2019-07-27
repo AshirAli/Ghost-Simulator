94c7ed50c0cd106640e5d68877e775488ede91be
AshirAli <ashirmayankalathil@gmail.com>
2019-07-27 18:46:19 +0530

    Ghost in wall effect - Custom Forward Renderer - LWRP upgraded to 10.16

--- Ghost Simulator Project/Assets/Scripts/GameManager.cs
+++ Ghost Simulator Project/Assets/Scripts/GameManager.cs
@@ -25,7 +25,7 @@ public class GameManager : MonoBehaviour{
     //Initializes the game for each level.
     void InitGame()
     {
-        SceneManager.LoadScene("Prototype-Scene");
+        SceneManager.LoadScene("Prototype-Shader-LWRP");
     }
 
     void Start(){
